2dcaba30e9eed110e3e01291730f94ef23e681b8
David123822 <iugadavidpetru99@yahoo.com>
2024-07-23 01:35:53 +0300

    negative test added

--- src/test/java/FailedLogin.java
+++ src/test/java/FailedLogin.java
@@ -8,11 +8,12 @@ import org.testng.annotations.*;
 
 import java.time.Duration; // Import Duration from java.time
 
+import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
-public class TestingLogin {
+import java.time.Duration;
 
-    String url = "http://127.0.0.1:8000/";
+public class FailedLogin {String url = "http://127.0.0.1:8000/";
     WebDriver driver;
 
     @BeforeTest
@@ -30,22 +31,9 @@ public class TestingLogin {
         }
     }
 
-    //@DataProvider(name = "loginData")
-    //public Object[][] loginData(){
-    //    return new Object[][]{
-    //            {"David", "qwerty21!"},//fail
-    //            {"Alice", "alicepwd"},
-    //            {"Mihail","test"}, // fail
-    //            {"Bob", "bobspassword"},//fail
-    //            {"david", "qwerty21!"},//pass
-    //             };
-    //}
-
     @Test
-    //@Test(dataProvider = "loginData")
-    @Parameters({"username", "password"}) // for the username-password.xml file (to run right-click the file and choose run)
-    public void accessThePage(String username, String password) throws InterruptedException {
-
+    @Parameters({"username","password"})
+    public void failedLogin(@Optional ("alice") String username,@Optional ("alicepassword")  String password){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Specify timeout using Duration
 
 
@@ -60,32 +48,17 @@ public class TestingLogin {
         WebElement passwordField = driver.findElement(By.id("id_password"));
         passwordField.sendKeys(password);
 
-        Timeoff(100);
 
         // Click the login button
         WebElement loginButton2 = driver.findElement(By.cssSelector(".btn.btn-primary[type='submit']"));
         loginButton2.click();
 
-        // Sleep for 2 seconds to wait for the next page to load
-        Timeoff(1000);
-
         // check if the url has changed
         String expected = "http://127.0.0.1:8000/topics/";
         String actual = driver.getCurrentUrl();;
 
-        // logging out
-        //WebElement logoutButton = driver.findElement(By.cssSelector(".btn.btn-outline-secondary.btn-sm[]"));
 
-        assertTrue("Login for username: " + username + " and password " + password, expected.contains(actual));
-
-        // Logout after successful login
-        // this will wait untill the logout button is visible
-        WebElement logoutButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[name='submit'].btn.btn-outline-secondary.btn-sm")));
-        logoutButton.click();
-    }
-
-    public void Timeoff(int miliseconds) throws InterruptedException {
-        Thread.sleep(miliseconds);
+        assertFalse("Login for username: " + username + " and password " + password, expected.contains(actual));
     }
 
 }
